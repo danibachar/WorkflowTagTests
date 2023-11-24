8ba32abd496b389dadc34eddc7fb1f4018af7c89
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-24 16:29:11 -0700

    update logging

--- deployment.py
+++ deployment.py
@@ -32,11 +32,11 @@ branch_prefix = 'release_'
 
 # fetch all tags
 tags = os.popen('git ls-remote --tags').read()
-logging.debug('tags {}'.format(tags))
+logging.info('tags {}'.format(tags))
 
 # parse tags and get the latest one
 tag_list = re.findall(r'refs/tags/(.+)', tags)
-logging.debug('tag_list {}'.format(tag_list))
+logging.info('tag_list {}'.format(tag_list))
 
 latest_tag = tag_list[-1] if tag_list else None
 if latest_tag is None:
@@ -44,7 +44,7 @@ if latest_tag is None:
 
 # clone the git repo
 os.system(f'git clone {repo_url}')
-logging.debug('cloning {}'.format(repo_url))
+logging.info('cloning {}'.format(repo_url))
 
 # navigate into the cloned repo (given the default clone command, the folder name would be the repository name)
 repo_name = repo_url.split('/')[-1].replace('.git', '')
@@ -54,12 +54,12 @@ os.chdir(repo_name)
 branch = latest_tag.replace('.', branch_prefix)
 # create and checkout new branch
 os.system(f'git checkout -b {branch}')
-logging.debug('new branch {}'.format(branch))
+logging.info('new branch {}'.format(branch))
 
 # create a new file
 new_formula_file_name = f"tuist@{latest_tag}.rb"
 os.system(f'touch {new_formula_file_name}')
-logging.debug('new homebrew formula {}'.format(new_formula_file_name))
+logging.info('new homebrew formula {}'.format(new_formula_file_name))
 
 # stage changes
 os.system('git add .')
