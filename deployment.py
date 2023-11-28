0999331162b54d5684b6dcaf3d6e85bb7b15e7da
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-28 15:36:03 -0700

    update user

--- deployment.py
+++ deployment.py
@@ -106,8 +106,10 @@ def _prepare_repo_locally():
     # navigate into the cloned repo (given the default clone command, the folder name would be the repository name)
     repo_name = repo_url.split('/')[-1].replace('.git', '')
     os.chdir(repo_name)
-    _run_command(f"git config --local user.email \"{os.environ.get("GIT_AUTHOR_EMAIL")}\"")
-    _run_command(f"git config --local user.name \"{os.environ.get("GITLAB_USER_ID")}\"") 
+    email = os.environ.get("GIT_AUTHOR_EMAIL")
+    user = os.environ.get("GITLAB_USER_ID")
+    _run_command(f"git config --local user.email {email}")
+    _run_command(f"git config --local user.name {user}")
 
 def _checkout_branch_by(tag):
     # create branch name from the latest tag
